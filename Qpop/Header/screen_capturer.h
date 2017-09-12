6f4250e9d6685cca966228f2bc8b15dd2d9a9fd1
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-09-12 00:52:06 -0700

    added take screenshot, changed to namespace

--- Qpop/Header/screen_capturer.h
+++ Qpop/Header/screen_capturer.h
@@ -2,15 +2,18 @@
 #define SCREEN_CAPTURER_H
 
 #include <iostream>
-#include <ancillary_functions.h>
+#include "ancillary_functions.h"
+#include "gdiplus.h"
+namespace Screen_Capturer{
 
-class Screen_Capturer{
-	public:
-		static struct result_data takeSnapshot(std::string p_name);
-		static struct result_data takeSnapshot(std::string p_name, struct result_data (*foo)(void*, void*), void* arg);
-		static float saveHBITMAP_SC(void* bmp, void* name);
-		static float compHBITMAP_WSC(void* sc_bmp, void* o_bmp);
-		static struct result_data compHBITMAP_SEL(void* sc_bmp, void* o_bmp);
-};
+	using namespace Ancillary_Function;
+
+	int takeSnapshot(Gdiplus::Bitmap** m, std::string p_name);
+	result_data takeSnapshot(std::string p_name, result_data (*foo)(void*, void*), void* arg);
+	float saveHBITMAP_SC(void* bmp, void* name);
+	float compHBITMAP_WSC(void* sc_bmp, void* o_bmp);
+	result_data compHBITMAP_SEL(void* sc_bmp, void* o_bmp);
+
+}
 
 #endif
