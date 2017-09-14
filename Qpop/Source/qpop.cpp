69dbbf7835ebc17721818614304d3683085f38d0
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-09-14 01:23:10 -0700

    functional revamp completed

--- Qpop/Source/qpop.cpp
+++ Qpop/Source/qpop.cpp
@@ -18,13 +18,13 @@
 #include <shared_access.h>
 #define RUNNING_DIR "./"
 
-Qpop_Server main_server;
-
 using namespace Ancillary_Function;
 
 void MonitorCondition(Qpop_Server* server, int x, int y, std::string exe_name);
-
+void SetDelayCount(unsigned int count);
+void DecDelayCount();
 int main(){
+	Qpop_Server main_server;
 	std::cout << "Starting Main Program" << std::endl;
 	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
    	ULONG_PTR gdiplusToken;
@@ -34,7 +34,7 @@ int main(){
 	Gdiplus::Bitmap* snapshot = NULL;
 	Gdiplus::Bitmap* bmp_from_file = NULL;
 	int result = 0;
-
+	bool local_delay;
    	// Create settings profile for league of legends
    	Settings_Profile cur_prof("lol.txt");
 
@@ -48,11 +48,11 @@ int main(){
 	// Start up server to connect with mobile app
 	main_server.set_port(8000);
 	main_server.start_server();
-
 	// Create thread to manage asynchronous requests. The numbers passed in are the coordinates for the
 	// ExhaustiveClick call. When conditions for the server are true - it recieves a signal from a phone - 
 	// it will call this exhaustive click
 	boost::thread monitor_thread(MonitorCondition, &main_server, cur_prof.getX(0) + (int) (bmp_from_file->GetWidth() / 2), cur_prof.getY(0) + (int) (bmp_from_file->GetHeight() / 2), exe_name);
+	
 	while(_close_qpop == 0){
 		result = Screen_Capturer::takeSnapshot(&snapshot, exe_name);
 		if(result != 0) std::cerr << "Error taking snapshot" << std::endl;
@@ -69,18 +69,28 @@ int main(){
 			// send message to mobile according to if the image matched
 			if(image_match){
 				condition.notify_one();
-				main_server.send("popped");
+				if(delay_count <= 0){
+					main_server.send("popped");
+					// If server is connected, set a 20 loop delay. This prevents a click being sent, the button transition
+					// not completing and retriggering the threshold, resulting in a short false positive
+					if(main_server.is_Connected()){
+						SetDelayCount(20);
+					}
+				}
 			}
 			else{
 				main_server.send("queued");
 				main_server.setCondition(false);
-				difference = 0;	
 			}
-
+			// deduct from delay count every iteration
+			if(delay_count > 0){
+				DecDelayCount();
+			}
+			std::cout << std::endl << "Delay Count: " << delay_count << std::endl;
 			std::cout << "Difference = " << difference << "%" << std::endl;
 		}
 
-		Sleep(800);
+		Sleep(500);
 	}
 
 	DeleteObject(bmp_from_file);
@@ -90,13 +100,40 @@ int main(){
 	return 0;
 }
 
-void MonitorCondition(Qpop_Server *s, int x, int y, std::string exe_name){
+/**
+ * Monitors the condition of whether or not
+ * @param s        pointer to a running Qpop_server
+ * @param x        x coordinate
+ * @param y        y coordinate
+ * @param exe_name name of process
+ */
+void MonitorCondition(Qpop_Server* s, int x, int y, std::string exe_name){
 	while(_close_qpop == 0){
 		boost::mutex::scoped_lock lock(_monitor_pause);
 		condition.wait(lock);
-		if(main_server.conditionSatisfied()){
+		if(s->conditionSatisfied()){
+			SetDelayCount(4);
 			ExhaustiveClick(x, y, exe_name);
-			main_server.setCondition(false);
+			s->setCondition(false);
 		}
 	}
+}
+
+/**
+ * Used to set the current delay count in the main while loop of qpop
+ * @param count number of loop iterations that must be made
+ */
+void SetDelayCount(unsigned int count){
+	_delay_after_click.lock();
+	delay_count = count;
+	_delay_after_click.unlock();
+}
+
+/**
+ * Decerement delay count
+ */
+void DecDelayCount(){
+	_delay_after_click.lock();
+	delay_count--;
+	_delay_after_click.unlock();	
 }
