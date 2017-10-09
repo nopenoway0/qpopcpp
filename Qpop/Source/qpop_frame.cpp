b705db362c4756a163354cf06b5c49c702b5b6c8
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-10-09 11:56:42 -0700

    added frame

--- Qpop/Source/qpop_frame.cpp
+++ Qpop/Source/qpop_frame.cpp
@@ -1,20 +1,19 @@
 #include "qpop_frame.h"
 namespace Qpop{
 	namespace gui{
-// Add containers to preven toverlapping
-QpopFrame::QpopFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(NULL, wxID_ANY, title, pos, size){
-	wxStaticText* status_button = new wxStaticText(this, wxSTATUS_TEXT, "connection");
-	wxStaticText* ip = new wxStaticText(this, wxIP_TEXT, "IPADDRESS: ");
-	wxStaticText* auth = new wxStaticText(this, wxAUTH_TEXT, "AUTHCODE: ");
-	wxStaticText* difference = new wxStaticText(this, wxDIFF_TEXT, "DIFFERENCE");
-	wxBoxSizer* layout = new wxBoxSizer(wxVERTICAL);
-	layout->Add(ip);
-	layout->AddSpacer(20);
-	layout->Add(auth);
-	layout->AddSpacer(20);
-	layout->Add(difference);
-	layout->AddSpacer(100);
-	layout->Add(status_button);
-	SetSizerAndFit(layout);
-}
+// TODO: CHANGE FONT, FIX SPACE, center text, add background
+		QpopFrame::QpopFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(NULL, wxID_ANY, title, pos, size) {
+			this->SetBackgroundColour(wxColour(0xF0, 0xFF, 0XFF));
+			wxStaticText* status_button = new wxStaticText(this, wxSTATUS_TEXT, "connection");
+			wxStaticText* auth = new wxStaticText(this, wxAUTH_TEXT, "AUTHCODE:");
+			wxStaticText* difference = new wxStaticText(this, wxDIFF_TEXT, "");
+			wxBoxSizer* layout = new wxBoxSizer(wxVERTICAL);
+
+			wxSizerFlags flags(2);
+			flags.Center();
+			layout->Add(auth, flags);
+			layout->Add(difference, flags);
+			layout->Add(status_button, flags);
+			SetSizer(layout);
+		}
 }}
